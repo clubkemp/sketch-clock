a3c472b6db291803144e816af799f3b7918d7f3b
Jonny K <Kempj2.jk@gmail.com>
2021-03-12 15:28:32 -0800

    changing color state on button click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 import AnalogClock from './components/AnalogClock/AnalogClock'
 import Draw from './components/Draw/Draw'
@@ -6,8 +6,9 @@ import Controls from './components/Controls/Controls';
 
 
 function App() {
+  const [color, setColor] = useState("22C55E")
   const handleColorChange = (e) =>{
-    console.log(e.target.value)
+    setColor(e.target.value)
   }
   return (
     <div className="App">
--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -5,8 +5,8 @@ export default function Controls({ handleColorChange }) {
     return (
         <div>
             <sl-button type="success" value="22C55E" onClick={handleColorChange}>Get Ready</sl-button>
-            <sl-button type="warning" value="F59E0B">Do</sl-button>
-            <sl-button type="danger" value="F59E0B">Done</sl-button>
+            <sl-button type="warning" value="F59E0B" onClick={handleColorChange}>Do</sl-button>
+            <sl-button type="danger" value="EF4444" onClick={handleColorChange}>Done</sl-button>
         </div>
     )
 }
